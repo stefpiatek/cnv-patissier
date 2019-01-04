2c4f06e5294974499ebab2a7f868a3b2c29706ff
Stef Piatek <stefan.piatek@nhs.net>
2019-01-04 09:33:05 +0000

    Custom running of canvas
    It does sucesfully run now, though the capture is too small for it to make any calls

--- example_settings.py
+++ example_settings.py
@@ -2,4 +2,5 @@ cnv_pat_settings = {
     "genome_fasta_path": "/var/reference_sequences/hg19/genome.fa",
     "max_cpu": "30",
     "max_mem": "50",
+    "http_proxy": None, # "http://192.168.1.1:8080/"
 }
