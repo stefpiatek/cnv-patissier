e8aa5497ca5fb0ca57e07ec9b644b8581c81cbcc
Stef Piatek <stefan.piatek@nhs.net>
2019-01-14 12:08:05 +0000

    Fix unit tests

--- tests/unit/test_base_classes.py
+++ tests/unit/test_base_classes.py
@@ -1,44 +1,62 @@
-from scripts.utils import get_cnv_patissier_dir
 from scripts.base_classes import BaseCNVTool
 
-cnv_pat_dir = get_cnv_patissier_dir()
+
+class TestGetBAMHeader:
+    pass
 
 
-class TestBaseCNVToolParseVCF:
+class TestParseVCF:
     def setup(self):
-        self.del_vcf = (
-            f"{cnv_pat_dir}/tests/test_files/output/cohort/date-run/caller/gene/extra-layers/17340-del_segments.vcf"
-        )
+        self.del_vcf = f"tests/test_files/output_parsing/vcf/15384-del_segments.vcf"
         self.del_expected_vcf_dict = {
-            "chrom": "chr17",
-            "start": "41251742",
-            "end": "41258601",
-            "id": "CNV_chr17_41251742_41258601",
+            "chrom": "chr7",
+            "start": "513245",
+            "end": "514245",
+            "id": "CNV_chr7_513245_514245",
             "ref": "N",
             "alt": "DEL",
             "qual": ".",
             "filter": ".",
-            "info": "END=41258601",
-            "format": "GT:CN:NP:QA:QS:QSE:QSS",
-            "data": "1:1:4:118:473:67:118",
-            "cnv_caller": "caller",
-            "gene": "gene",
-            "sample_id": "17340-del",
-            "cohort": "cohort",
+            "info_data": {"END": "514245"},
+            "format_data": {"GT": "1", "CN": "1", "NP": "4", "QA": "118", "QS": "473", "QSE": "67", "QSS": "118"},
+            "sample_id": "15384-del",
         }
 
     def test_del(self):
-        vcf_dict = BaseCNVTool.parse_vcf_4_2(self.del_vcf)
+        with open(self.del_vcf) as handle:
+            vcf_dict = BaseCNVTool.parse_vcf_4_2(handle, "15384-del")
         assert vcf_dict[0] == self.del_expected_vcf_dict
 
     def test_dup(self):
         expected = dict(self.del_expected_vcf_dict)
         expected["alt"] = "DUP"
-        expected["data"] = "2:3:4:118:473:67:118"
-        expected["sample_id"] = "17340-dup"
-        vcf_dict = BaseCNVTool.parse_vcf_4_2(self.del_vcf.replace("17340-del", "17340-dup"))
+        expected["format_data"] = {"GT": "2", "CN": "3", "NP": "4", "QA": "118", "QS": "473", "QSE": "67", "QSS": "118"}
+        expected["sample_id"] = "15384-dup"
+        with open(self.del_vcf.replace("15384-del", "15384-dup")) as handle:
+            vcf_dict = BaseCNVTool.parse_vcf_4_2(handle, "15384-dup")
         assert vcf_dict[0] == expected
 
     def test_no_cnv(self):
-        vcf_dict = BaseCNVTool.parse_vcf_4_2(self.del_vcf.replace("17340-del", "17340-none"))
+        with open(self.del_vcf.replace("15384-del", "15384-none")) as handle:
+            vcf_dict = BaseCNVTool.parse_vcf_4_2(handle, None)
         assert vcf_dict == []
+
+
+class TestUploadCNVCaller:
+    pass
+
+
+class TestUploadGene:
+    pass
+
+
+class TestUploadSamples:
+    pass
+
+
+class TestUploadPositiveCNVs:
+    pass
+
+
+class TestUploadCalledCNVs:
+    pass
