95dcb9467586df8a926f69cfc4ef1e05c509c9bf
Stef Piatek <stefan.piatek@nhs.net>
2019-06-04 14:48:59 +0100

    Start poster

--- cnv-caller-resources/reports/helper_functions/database.R
+++ cnv-caller-resources/reports/helper_functions/database.R
@@ -9,7 +9,18 @@ get_callers <- function(con){
   callers <- con %>%
     tbl("callers") %>%
     collect() %>%
-    mutate(name = str_remove(name, "_case")) 
+    mutate(name = str_remove(name, "_case")) %>%
+    mutate(name =str_replace(name, "cnvkit", "CNV kit")) %>%
+    mutate(name =str_replace(name, "codex2", "CODEX2")) %>%
+    mutate(name =str_replace(name, "copywriter", "CopywriteR")) %>%
+    mutate(name =str_replace(name, "decon", "DECoN")) %>%
+    mutate(name =str_replace(name, "exome-depth", "ExomeDepth")) %>%
+    mutate(name =str_replace(name, "excavator2", "EXCAVATOR2")) %>%
+    mutate(name =str_replace(name, "gatk", "GATK gCNV")) %>%
+    mutate(name =str_replace(name, "panelcn_mops", "panelcn.MOPS")) %>%
+    mutate(name =str_replace(name, "savvycnv", "SavvyCNV")) %>%
+    mutate(name =str_replace(name, "xhmm", "XHMM")) 
+    
   
   
   return(callers)
@@ -38,8 +49,8 @@ get_true_positives <- function(db_file){
     left_join(tbl(con, "called_cnvs"), by=c("id" = "sample_id"),  suffix = c("", "_called_cnv")) %>%
     collect() %>%
     mutate(sample_id = id) %>%
-    filter(!(capture == "MiniPanel" & name_gene == "BRCA1" & cnv_id == 37)) %>%
-    filter(!(capture == "MiniPanel" & name_gene == "BRCA2" & cnv_id == 115)) %>%
+    filter(!(capture == "MiniPanel" & name_gene == "BRCA1" & start == 41197636 & end == 41276193 & cnv_id == 66)) %>%
+    filter(!(capture == "MiniPanel" & name_gene == "BRCA2" & start == 32890510 & end == 32972962 & cnv_id == 115)) %>%
     select(-id) %>%
     group_by(caller_id, sample_id, name_gene) %>%
     slice(which.min(id_called_cnv)) %>%
@@ -83,8 +94,9 @@ get_false_positives <- function(db_file){
     left_join(tbl(con, "called_cnvs"), by=c("id" = "sample_id"),  suffix = c("", "_called_cnv")) %>%
     collect() %>%
     mutate(sample_id = id) %>%
-    filter(!(capture == "MiniPanel" & name_gene == "BRCA1" & cnv_id == 37)) %>%
-    filter(!(capture == "MiniPanel" & name_gene == "BRCA2" & cnv_id == 115)) %>%
+    filter(!(capture == "MiniPanel" & name_gene == "BRCA1" & start == 41197636 & end == 41276193 & cnv_id == 66)) %>%
+    filter(!(capture == "MiniPanel" & name_gene == "BRCA2" & start == 32890510 & end == 32972962 & cnv_id == 115)) %>%
+    filter(!(capture == "ICR" & name_gene != "BRCA1" & name_gene != "BRCA2")) %>%
     select(-id) %>%
     group_by(caller_id, sample_id, name_gene) %>%
     slice(which.min(id_called_cnv)) %>%
@@ -105,6 +117,7 @@ get_negatives <- function(db_file){
   negatives <-  genes %>%
     left_join(tbl(con, "samples"), by = c("id" = "gene_id"), suffix = c("_gene", "")) %>%
     filter(result_type == "normal") %>% 
+    filter(!(capture == "ICR" & name_gene != "BRCA1" & name_gene != "BRCA2")) %>%
     collect() 
   
   dbDisconnect(con)
